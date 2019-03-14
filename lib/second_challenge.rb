4a027c2ffed42d167cc2d91733476a063e55576e
Adam S <adam.sarli@gmail.com>
2019-03-14 15:59:56 +0000

    Automatically backed up by Learn

--- lib/second_challenge.rb
+++ lib/second_challenge.rb
@@ -1,7 +1,8 @@
 def second_challenge
   epic_tragedy = {
     :montague => {
-      #your key/value pairs here!
+      partriarch: {}, :matriarch => {}, hero: {}, 
+      :hero_friends => []
       }, 
     :capulet => {
       #your key/value pairs here!
